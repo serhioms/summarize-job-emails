b134148c197a9dc0abf0e8f4e1e50b37bf582605
Sergey Moskovskiy <sergey.moskovskiy@gmail.com>
2026-06-22 18:12:17 -0400

    Initial commit

---
+++ src/main/java/com/openclaw/summarize/SummarizeJobEmailsApplication.java
@@ -0,0 +1,12 @@
+package com.openclaw.summarize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummarizeJobEmailsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummarizeJobEmailsApplication.class, args);
+    }
+}
